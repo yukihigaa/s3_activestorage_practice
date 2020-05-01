6342de0fa183b907d4e3ec5daa473ae53b20a0e0
yukihigaa <uky02220@gmail.com>
2020-05-02 03:09:01 +0900

    images validates

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,16 @@
 class Tweet < ApplicationRecord
+
     has_many_attached :images
+    validate :chack_content_type
+
+    def chack_content_type
+        return '' unless images.attached?
+        images.each do |image|
+            if !image.content_type.in?(%('image/jpeg image/png'))
+                image.purge
+                errors.add(:images, '画像ファイルしかアップできません')
+            end
+        end
+    end
+
 end
